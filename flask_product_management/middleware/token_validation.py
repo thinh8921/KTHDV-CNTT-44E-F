83c7c4ae36fb1c1f99ea7a783280e095e61bd2bc
thinh8921 <132537084+thinh8921@users.noreply.github.com>
2024-12-08 16:42:36 +0700

    Create token_validation.py

---
+++ flask_product_management/middleware/token_validation.py
@@ -0,0 +1,12 @@
+from flask_jwt_extended import verify_jwt_in_request
+from flask import jsonify
+
+def token_required(f):
+    def wrapper(*args, **kwargs):
+        try:
+            verify_jwt_in_request()
+        except Exception:
+            return jsonify(message="Token validation failed"), 401
+        return f(*args, **kwargs)
+    wrapper.__name__ = f.__name__
+    return wrapper
